0c2716bdbaf49082c09b3f93f6ea12b8df3fbcba
Pedro Teixeira <pedroluisflorentino@gmail.com>
2024-08-23 16:16:08 -0300

    Terraform MGC - Basic Configuration

---
+++ networking.tf
@@ -0,0 +1,4 @@
+resource "mgc_network_vpc" "vpc_pedro" {
+    name = "VPC-Pedro-TF"
+    description = "VPC criada via Terraform"
+}
--- providers.tf
+++ providers.tf
@@ -2,22 +2,22 @@ provider "mgc" {
     region="br-se1"
     alias = "sudeste"
     # api_key = "b0485d50-6706-4567-9479-217b406747de"
-    # object_storage = {
-    #     key_pair = {
-    #         key_id = "297ec078-c593-400c-8ea7-0bdb0140ba42"
-    #         key_secret = "4884f5be-3263-40c8-844d-fa5abab087c9"
-    #     }
-    # }
+    object_storage = {
+        key_pair = {
+            key_id = "297ec078-c593-400c-8ea7-0bdb0140ba42"
+            key_secret = "4884f5be-3263-40c8-844d-fa5abab087c9"
+        }
+    }
 }
 
 provider "mgc" {
     region="br-ne1"
     alias = "nordeste"
     # api_key = "b0485d50-6706-4567-9479-217b406747de"
-    # object_storage = {
-    #     key_pair = {
-    #         key_id = "297ec078-c593-400c-8ea7-0bdb0140ba42"
-    #         key_secret = "4884f5be-3263-40c8-844d-fa5abab087c9"
-    #     }
-    # }
+    object_storage = {
+        key_pair = {
+            key_id = "297ec078-c593-400c-8ea7-0bdb0140ba42"
+            key_secret = "4884f5be-3263-40c8-844d-fa5abab087c9"
+        }
+    }
 }
